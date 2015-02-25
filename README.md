2fe0cd08c51bdf0ece700fe1fe32145f637655e4
Ricardo Corrie <rcorrie90@gmail.com>
2015-02-25 01:54:48 -0500

    Init

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# scroll-toggle
-ScrollToggle applies classes to an element depending on scrolling direction
+# ScrollToggle
+ScrollToggle is a small component that lets you apply classes to an element depending on which direction the document is scrolled.
+## Documentation:
+ 
+### General Overview
+       
+       ScrollToggle is a small component that lets you apply classes to an
+       element depending on which direction the document is scrolled.
+
+### Use case
+```javascript
+       // To instantiate a new ScrollToggle element:
+
+       var foo = new ScrollToggle('.class-name-for-element');
+
+       // OR
+
+       var foo = new document.getElementById('element-id').ScrollToggle();
+
+       // To initialize listner:
+
+       foo.init();
+
+       // You can also pass custom options like so:
+
+       foo.init({offset: 500});
+
+       // You can halt ScrollToggle instance:
+
+       foo.halt();
+```
+### Options
+
+       scrollClass:      {string} applies passed class name to element on init
+
+       scrollUpClass:    {string} applies passed class name to element on upwards scroll
+
+       scrollDownClass:  {string} applies passed class name to element on downward scroll
+
+       offset:           {integer} ignores scrolling between top margin and offset
+
+### Default Options
+       
+       scrollClass:      'scroll-toggle'
+
+       scrollUpClass:    'scroll-toggle-up'
+
+       scrollDownClass:  'scroll-toggle-down'
+
+       offset:           0
+
---
+++ scroll-toggle.js
@@ -0,0 +1,115 @@
+/**
+*
+* Author:      Ricardo Corrie <https://github.com/rcorrie
+* version:     scroll-toggle 0.0.1
+* Modified:    Ricardo Corrie, 02/25/2015
+*
+* Documentation:
+* 
+*   -- General Overview
+*       
+*       ScrollToggle is a small component that lets you apply classes to an
+*       element depending on which direction the document is scrolled.
+*
+*   -- Use case
+*
+*       // To instantiate a new ScrollToggle element:
+*
+*       var foo = new ScrollToggle('.class-name-for-element');
+*
+*       // OR
+*
+*       var foo = new document.getElementById('element-id').ScrollToggle();
+*
+*       // To initialize listner:
+*
+*       foo.init();
+*
+*       // You can also pass custom options like so:
+*
+*       foo.init({offset: 500});
+*
+*       // You can halt ScrollToggle instance:
+*
+*       foo.halt();
+*
+*   -- Options
+*
+*       scrollClass:      {string} applies passed class name to element on init
+*
+*       scrollUpClass:    {string} applies passed class name to element on upwards scroll
+*
+*       scrollDownClass:  {string} applies passed class name to element on downward scroll
+*
+*       offset:           {integer} ignores scrolling between top margin and offset
+*
+*   -- Default Options
+*       
+*       scrollClass:      'scroll-toggle'
+*
+*       scrollUpClass:    'scroll-toggle-up'
+*
+*       scrollDownClass:  'scroll-toggle-down'
+*
+*       offset:           0
+*
+*/
+
+;(function(def){
+
+  window.ScrollToggle = def;
+  Object.prototype.scrollToggle = def;
+
+})(function(element){
+
+  var _element = ( element  ? document.querySelector(element) : this );
+
+  var lastScrollTop = 0;
+
+  var scrollOptions = {
+    scrollClass:      'scroll-toggle',
+    scrollUpClass:    'scroll-toggle-up',
+    scrollDownClass:  'scroll-toggle-down',
+    offset:           0
+  };
+
+  // Private Methods
+
+  var scrollListener = function(){
+    var st = document.body.scrollTop;
+    var scroll = st > lastScrollTop;
+    if(st > scrollOptions.offset) applyToggle(scroll);
+    lastScrollTop = st;
+  };
+
+  var applyToggle = function(scroll){
+    _element.classList.remove( ( scroll ? scrollOptions.scrollUpClass : scrollOptions.scrollDownClass ) );
+    _element.classList.add( ( scroll ? scrollOptions.scrollDownClass : scrollOptions.scrollUpClass ) );
+  };
+
+  var applyOptions = function(options){
+    for(var key in options){
+      scrollOptions[key] = options[key];
+    };
+  };
+
+  // Public Methods
+
+  function initScrollToggle(options){
+    applyOptions(options);
+    _element.classList.add(scrollOptions.scrollClass);
+    setTimeout(function(){
+      window.addEventListener('scroll', scrollListener);
+    }, 500)
+  };
+
+  function haltScrollToggle(){
+    window.removeEventListener('scroll', scrollListener);
+  };
+
+  return {
+    init: initScrollToggle,
+    halt: haltScrollToggle
+  };
+
+});
